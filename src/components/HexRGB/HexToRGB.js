12a04582788106628324ecbb427785ea9830b63d
avk001 <avk001@yandex.ru>
2020-12-14 11:13:47 +0300

    function checkHex

--- src/components/HexRGB/HexToRGB.js
+++ src/components/HexRGB/HexToRGB.js
@@ -1,10 +1,15 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
+function checkHex(hex) {
+    const hexRegex = /^[#]*([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i
+    return hexRegex.test(hex);
+}
+
 function HexToRGB(props) {
     const hexArray = Array.from(props.hexCode);
     let rgb=null;
-    if(hexArray.length === 7 && hexArray[0] === '#') {
+    if(checkHex(props.hexCode) ) {
         let r = parseInt(hexArray[1] + hexArray[2], 16);
         let g = parseInt(hexArray[3] + hexArray[4], 16);
         let b = parseInt(hexArray[5] + hexArray[6], 16);
@@ -13,12 +18,12 @@ function HexToRGB(props) {
 
         let form = document.querySelector('.wrap-background');
         rgb !== -1 ? form.setAttribute('style', `background-color: rgba(${r}, ${g}, ${b}, 0.5`) : form.setAttribute('style', 'background-color: rgba(255, 0, 0, 0.6)' )
-    }else if(hexArray.length === 0 )
-    {
+    }else if(hexArray.length === 0 ){
         let form = document.querySelector('.wrap-background');
 
         if(form) form.setAttribute('style', `background-color: rgba(0, 0, 0, 0.5`)
-        // rgb=0;
+    }else{
+        rgb = -1
     }
 
     let SetRGBStyle = {
